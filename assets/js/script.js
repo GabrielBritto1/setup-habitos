e8ba0b96a65d46a9d177ffc874957bda57471965
Gabriel <gabriel_bianchibritto@hotmail.com>
2023-01-22 11:42:48 -0300

    adição do input.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,10 +2,10 @@ const form = document.querySelector("#form-habits")
 const nlwSetup = new NLWSetup(form)
 
 const data = {
-    run: ["01-01", "01-02", "01-06"],
+    run: ['01-01', '01-02', '01-06', '01-07', '01-08'],
+    water: ['01-03'],
+    study: ['01-02'],
 }
 
 nlwSetup.setData(data)
 nlwSetup.load()
-
-console.log(nlwSetup)
