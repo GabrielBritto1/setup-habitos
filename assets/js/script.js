6ab6fc681e1e7b64727242ea5cf7fb0ab2ac359a
Gabriel <gabriel_bianchibritto@hotmail.com>
2023-01-22 12:53:41 -0300

    inserindo função ao botão e saves

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,33 @@
 const form = document.querySelector("#form-habits")
 const nlwSetup = new NLWSetup(form)
+const button = document.querySelector('header button')
 
-const data = {
-    run: ['01-01', '01-02', '01-06', '01-07', '01-08'],
-    water: ['01-03'],
-    study: ['01-02'],
+button.addEventListener('click', criarDia)
+form.addEventListener('change', save)
+
+
+function criarDia() {
+
+    const today = new Date().toLocaleDateString('pt-br').slice(0, -5)
+    const dayExists = nlwSetup.dayExists(today)
+
+    if (dayExists) {
+        alert('Day already exist')
+        return
+    }
+
+    alert('Add with success')
+    nlwSetup.addDay(today)
 }
+
+
+function save() {
+    localStorage.setItem('setupBritto@habits', JSON.stringify(nlwSetup.data));
+}
+
+
+const data = JSON.parse(localStorage.getItem('setupBritto@habits')) || {};
+
 
 nlwSetup.setData(data)
 nlwSetup.load()
